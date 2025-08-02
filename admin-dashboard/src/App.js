5214aefb6c2007eadad19d37b3d2fbd62d10ab19
Alex Benson <alexbensonux@gmail.com>
2025-08-02 15:05:54 -0700

    3.4 Define Routes Using Routes and Route Components

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,26 +1,27 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Clients from './components/Clients';
+import ClientDetail from './components/ClientDetail';
+import Trainers from './components/Trainers';
+import TrainerDetail from './components/TrainerDetail';
+import Movements from './components/Movements';
+import MovementDetail from './components/MovementDetail';
+import Admin from './components/Admin';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/client/:id" element={<ClientDetail />} />
+        <Route path="/trainer" element={<Trainers />} />
+        <Route path="/trainer/:id" element={<TrainerDetail />} />
+        <Route path="/movement" element={<Movements />} />
+        <Route path="/movement/:id" element={<MovementDetail />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </Router>
   );
 }
