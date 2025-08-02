ef31af95261529bf54d5f9c799a2ef9c857993c8
Alex Benson <alexbensonux@gmail.com>
2025-08-02 15:08:12 -0700

    Task 3 complete: Routing and Shell Views

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
 import Home from './components/Home';
 import Clients from './components/Clients';
 import ClientDetail from './components/ClientDetail';
@@ -9,18 +9,48 @@ import MovementDetail from './components/MovementDetail';
 import Admin from './components/Admin';
 import './App.css';
 
+function NotFound() {
+  return <div>Page Not Found</div>;
+}
+
+function ClientDetailWithParams() {
+  const { id } = useParams();
+  return <div>Client Detail for ID: {id}</div>;
+}
+
+function TrainerDetailWithParams() {
+  const { id } = useParams();
+  return <div>Trainer Detail for ID: {id}</div>;
+}
+
+function MovementDetailWithParams() {
+  const { id } = useParams();
+  return <div>Movement Detail for ID: {id}</div>;
+}
+
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/clients">Clients</Link> |{' '}
+        <Link to="/client/123">Client 123</Link> |{' '}
+        <Link to="/trainer">Trainers</Link> |{' '}
+        <Link to="/trainer/456">Trainer 456</Link> |{' '}
+        <Link to="/movement">Movements</Link> |{' '}
+        <Link to="/movement/789">Movement 789</Link> |{' '}
+        <Link to="/admin">Admin</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/clients" element={<Clients />} />
-        <Route path="/client/:id" element={<ClientDetail />} />
+        <Route path="/client/:id" element={<ClientDetailWithParams />} />
         <Route path="/trainer" element={<Trainers />} />
-        <Route path="/trainer/:id" element={<TrainerDetail />} />
+        <Route path="/trainer/:id" element={<TrainerDetailWithParams />} />
         <Route path="/movement" element={<Movements />} />
-        <Route path="/movement/:id" element={<MovementDetail />} />
+        <Route path="/movement/:id" element={<MovementDetailWithParams />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
